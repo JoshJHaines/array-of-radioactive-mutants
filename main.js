09ce3ac3dd0df104e2d0eb7b8728cbf1506f144f
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-11 16:38:29 -0500

    created 'oddOrEven' function. Passing test for function.

--- main.js
+++ main.js
@@ -19,6 +19,16 @@ function changeAllValuesTo(arr, val){
   }
 }
 
+function oddOrEven(arr){
+  for (i=0; i<arr.length; i++){
+    if (arr[i] % 2 === 0){
+      arr[i] ='even'
+    } else {
+      arr[i] = 'odd'
+    }
+  }
+}
+
 
 
 /*********************************
